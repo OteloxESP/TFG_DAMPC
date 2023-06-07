57034eccbfd2905fc8341a362735764568e83b20
Jose Antonio Orellana Gomez <101654298+OteloxESP@users.noreply.github.com>
2023-06-07 16:13:35 +0200

    Avances login

--- src/main/java/com/example/tfg_dampc/LoginController.java
+++ src/main/java/com/example/tfg_dampc/LoginController.java
@@ -6,11 +6,10 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.bson.Document;
+import org.controlsfx.dialog.ProgressDialog;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.io.IOException;
@@ -27,6 +26,9 @@ public class LoginController {
     private Button loginButton;
 
     @FXML
+    private Label errorLabel;
+
+    @FXML
     private void initialize() {
         // Inicializar controlador
     }
@@ -40,44 +42,91 @@ public class LoginController {
                 .applyConnectionString(new ConnectionString(conexionURL))
                 .serverApi(serverApi)
                 .build();
-        // Create a new client and connect to the server
+
+        // Crear un cliente y conectarlo al servidor.
         try (MongoClient mongoClient = MongoClients.create(settings)) {
             try {
-                MongoDatabase database = mongoClient.getDatabase("bdoHelp");
-                MongoCollection<Document> collection = database.getCollection("Usuarios");
+                MongoDatabase database = mongoClient.getDatabase("bdoHelp"); // Obtener base de datos
+                MongoCollection<Document> collection = database.getCollection("Usuarios"); // Obtener la colección de usuarios
+
+                // Obtener los valores de los campos
                 String usuario = usernameField.getText();
-                String contraseña = passwordField.getText();
+                String contrasena = passwordField.getText();
 
-                // Buscar el usuario y contraseña en la colección
-                Document query = new Document("usuario", usuario);
-                Document user = collection.find(query).first();
+                // Comprobar si los campos están vacíos
+                if (usuario.isEmpty() || contrasena.isEmpty()) {
+                    mostrarErrorLabel("Ingrese el usuario y la contraseña"); // Mostrar label de aviso al usuario
 
-                if (user != null) {
-                    String contraseñaDB = user.getString("contraseña");
-
-                    if (BCrypt.checkpw(contraseña, contraseñaDB)) {
-                        System.out.println("Inicio de sesión exitoso");
-                        try {
-                            Stage stage = new Stage();
-                            InicioApplication inicio = new InicioApplication();
-                            inicio.start(stage);
-                            loginButton.getScene().getWindow().hide();
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                    } else {
-                        System.out.println("Credenciales incorrectas 2");
-                    }
                 } else {
-                    System.out.println("Credenciales incorrectas 1");
-                }
+                    ocultarErrorLabel(); //Ocultar label al usuario y quitar estilos a los campos
 
+                    // Buscar el usuario y contrasena en la colección
+                    Document query = new Document("usuario", usuario);
+                    Document user = collection.find(query).first();
+
+                    // Comprobar si el usuario coincide
+                    if (user != null) {
+                        String contrasenaDB = user.getString("contraseña");
+
+                        // Comprobar si la contrasena coincide
+                        if (BCrypt.checkpw(contrasena, contrasenaDB)) {
+                            System.out.println("Inicio de sesión exitoso");
+                            try {
+                                // Crear la nueva ventana y ocultar la actual
+                                Stage stage = new Stage();
+                                InicioApplication inicio = new InicioApplication();
+                                inicio.start(stage);
+                                loginButton.getScene().getWindow().hide();
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                ocultarErrorLabel(); //Ocultar label al usuario y quitar estilos a los campos
+                                mostrarErrorAlerta("Error de servidor"); //Mostrar alerta al usuario
+                            }
+
+                        } else {
+                            System.out.println("Credenciales incorrectas 2");
+                            mostrarErrorLabel("No existe un usuario con ese usuario y/o contraseña"); // Mostrar label de aviso al usuario
+                        }
+                    } else {
+                        System.out.println("Credenciales incorrectas 1");
+                        mostrarErrorLabel("No existe un usuario con ese usuario y/o contraseña"); // Mostrar label de aviso al usuario
+                    }
+                }
 
             } catch (MongoException e) {
                 e.printStackTrace();
+                ocultarErrorLabel(); //Ocultar label al usuario y quitar estilos a los campos
+                mostrarErrorAlerta("Error de servidor"); //Mostrar alerta al usuario
             }
         }
+    }
+
+    public void mostrarErrorLabel(String titulo){
+        errorLabel.setText(titulo);
+        errorLabel.setVisible(true);
+        errorLabel.setManaged(true);
+
+        // Añadir los bordes rojos a los campos
+        usernameField.setStyle("-fx-border-color: red;");
+        passwordField.setStyle("-fx-border-color: red;");
+    }
+
+    public void ocultarErrorLabel(){
+        // Restaurar estilo de los campos de texto
+        usernameField.setStyle("");
+        passwordField.setStyle("");
+
+        // Ocultar el errorLabel
+        errorLabel.setVisible(false);
+        errorLabel.setManaged(false);
+    }
+
+    public void mostrarErrorAlerta(String titulo){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error de servidor");
+        alert.setHeaderText(null);
+        alert.setContentText(titulo);
+        alert.show();
     }
 }
