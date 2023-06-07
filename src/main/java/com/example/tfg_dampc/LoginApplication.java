488dea0624c19a104d95b6f1847c943d1cf5461d
Jose Antonio Orellana Gomez <101654298+OteloxESP@users.noreply.github.com>
2023-06-07 15:00:34 +0200

    Logo de la aplicacion

--- src/main/java/com/example/tfg_dampc/LoginApplication.java
+++ src/main/java/com/example/tfg_dampc/LoginApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +13,10 @@ public class LoginApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
-        Scene scene = new Scene(root, 420, 340);
+        Scene scene = new Scene(root, 400, 340);
 
         stage.setTitle("Inicio de sesión");
+        stage.getIcons().add(new Image(LoginApplication.class.getResourceAsStream("logo.png")));
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
